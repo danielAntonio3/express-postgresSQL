5fc537e618b8235f20d4a8c45ea30d990a9b3b88
Daniel Antonio <49245656+danielAntonio3@users.noreply.github.com>
2022-06-27 02:02:12 -0500

    como agregar un nuevo campo a la tabla

--- models/user.model.js
+++ models/user.model.js
@@ -22,6 +22,11 @@ const UserSchema = {
     allowNull: false,
     type: DataTypes.STRING,
   },
+  role: {
+    allowNull: false,
+    type: DataTypes.ENUM('customer', 'admin', 'soporte'),
+    defaultValue: 'customer',
+  },
   createdAt: {
     allowNull: false,
     type: DataTypes.DATE,
--- schemas/user.js
+++ schemas/user.js
@@ -4,17 +4,20 @@ const id = joi.string();
 const name = joi.string();
 const email = joi.string();
 const password = joi.string();
+const role = joi.string();
 
 const createUserSchema = joi.object({
   name: name.required(),
   email: email.required(),
   password: password.required(),
+  role: role.required(),
 });
 
 const updateUserSchema = joi.object({
   name,
   email,
   password,
+  role,
 });
 
 const getUserSchema = joi.object({
